3703aade02391c82547ec81a3afb03655a6acb50
samishegga <47670429+samishegga@users.noreply.github.com>
2019-03-17 15:51:24 +0300

    SeekBar Example with TextView Resizing by draging Seekbar. Display message for all three methods of SeekBar using Toast. The TextView that is being resize is "Sami"..

--- app/src/main/java/com/example/app10/MainActivity.java
+++ app/src/main/java/com/example/app10/MainActivity.java
@@ -6,33 +6,57 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static ListView list_view;
-    private static String names[]={"Ali", "Mohmd", "Usman", "Ahmed","Chala"};
+private static SeekBar seekBar;
+private static TextView text_seek, text_sami;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List_View();
+        SeekBar_Method();
+
     }
 
+public void SeekBar_Method(){
+    seekBar=(SeekBar)findViewById(R.id.seekBar);
+    text_seek=(TextView)findViewById(R.id.textView_seekbar);
+    text_sami=(TextView)findViewById(R.id.textView_Sami);
+    text_seek.setText("Covered: "+ seekBar.getProgress()+" / "+seekBar.getMax());
 
-    public void List_View(){
-        list_view=(ListView) findViewById(R.id.ListView_id);
+    seekBar.setOnSeekBarChangeListener(
+            new SeekBar.OnSeekBarChangeListener() {
+                int progress_value;
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    progress_value=progress;
+                    text_seek.setText("Covered: "+ progress +" / "+seekBar.getMax());
+                    text_sami.setTextSize(progress*2);
+                    Toast.makeText(MainActivity.this,"SeekBar in Progress", Toast.LENGTH_LONG).show();
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.name_list,names);
-        list_view.setAdapter(adapter);
+                }
 
-        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String value=(String)list_view.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this,"Position: "+position+" Value: "+value,
-                        Toast.LENGTH_LONG).show();
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(MainActivity.this,"SeekBar in StartTrackingTouch", Toast.LENGTH_LONG).show();
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    text_seek.setText("Covered: "+ progress_value +" / "+seekBar.getMax());
+                    Toast.makeText(MainActivity.this,"SeekBar in StopTrackingTouch", Toast.LENGTH_LONG).show();
+
+
+                }
             }
-        });
-    }
+    );
+
+
+}
 }
